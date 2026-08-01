cb6f34c08b507ed0eba3e71dd2a8b5629513b8de
fit-force23 <hb22a130@oecu.jp>
2026-08-01 11:43:41 +0900

    Add service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BENCH125 Service Worker — PWAとして正式インストール可能にする + オフライン対応 */
-const CACHE = "bench125-v11";
+const CACHE = "bench125-v12";
 const CORE = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png", "icon-180.png"];
 
 self.addEventListener("install", e => {
