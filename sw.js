9bb5c98c344b0b3d9dd45b6d8d229111b381f5bf
fit-force23 <hb22a130@oecu.jp>
2026-08-04 00:40:20 +0900

    Add service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* OVERLOAD Service Worker — PWAとして正式インストール可能にする + オフライン対応 */
-const CACHE = "overload-v16";
+const CACHE = "overload-v17";
 const CORE = ["./", "index.html", "manifest.json", "icon-192.png", "icon-512.png", "icon-180.png"];
 
 self.addEventListener("install", e => {
